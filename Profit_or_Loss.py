3f082066e61542b46eb243512712c62bf2fa9e9b
Manasa <143861749+manasasuryasubrahmanayeswari@users.noreply.github.com>
2023-11-05 20:09:26 +0530

    c=int(input()) s=int(input()) if(s>c):   {       print("Profit")   } elif(s<c):     {         print("Loss")     } else:     {         print("No Profit and No Loss")     }

---
+++ Profit_or_Loss.py
@@ -0,0 +1,14 @@
+c=int(input())
+s=int(input())
+if(s>c):
+  {
+      print("Profit")
+  } 
+elif(s<c):
+    {
+        print("Loss")
+    }
+else:
+    {
+        print("No Profit and No Loss")
+    }
